74944271047ae7b6bb5566b8ef78503a90d0310d
Derek Graham <mrsmond@users.noreply.github.com>
2016-05-04 18:43:45 +0100

    Added another move function that searches the state space. No heuristics so suffers from massive state explosion. Also added command line args to control lots of options.

--- hanabi_dgraham.py
+++ hanabi_dgraham.py
@@ -1,6 +1,10 @@
 from hanabi import *
 from itertools import chain, repeat
 from operator import itemgetter
+from simpleai.search import SearchProblem, breadth_first
+from simpleai.search.viewers import *
+from copy import deepcopy
+
 
 FULL_SET = [(c, v) for c in COLOURS for v in VALUES]
 CARDS_PER_VALUE = [v for v in VALUES for i in xrange(VALUES_COUNT[v - 1])]
@@ -38,6 +42,126 @@ def prune(my_hand, ids, colour_or_value):
                 my_hand[id] = [c for c in my_hand[id] if c[0] != colour_or_value]
             else:
                 my_hand[id] = [c for c in my_hand[id] if c[1] != colour_or_value]
+
+def build_possible_hands(game, current_player):
+    """Return the possibilities for each card in every other player's
+    hand from the perspective of the `current_player'.
+    """
+    hand_possibilities = {}
+    whats_left = {}
+    for p, h in game["players"].iteritems():
+        # Initialise what each of the players cards could be
+        hand_possibilities[p] = dict([(id, list(FULL_SET)) for id in h])
+
+        # Initialise all the possible cards which we will then update
+        # as we assess the game
+        whats_left[p] = dict([(c, list(CARDS_PER_VALUE)) for c in COLOURS])
+
+        # Remove what's in other players' hands and what's been played
+        # or discarded. Since we don't know the current player's hand
+        # then we have to make an inexact picture of what *other*
+        # players know
+        all_cards = list(chain(chain.from_iterable([h1 for p1, h1 in game["players"].iteritems() \
+                                                    if (p1 != p and p1 != current_player)]),
+                               game["played"], game["discarded"]))
+        for (colour, value, id) in all_cards:
+            whats_left[p][colour].remove(value)
+
+        # Rule out from each player's hand any card not possible from
+        # what's left
+        for colour, values in whats_left[p].iteritems():
+            for v in VALUES:
+                if not v in values:
+                    rule_out(hand_possibilities[p], colour, v)
+
+        # Update the hand with any clues given to this player
+        for clue_data in [m[1]["data"] for m in game["moves"] if m[1]["type"] == "clue" and \
+                          m[1]["data"][0] == p]:
+            prune(hand_possibilities[p], clue_data[2], clue_data[1])
+
+    return hand_possibilities
+
+def all_moves(game, current_player):
+    """Return all moves for the current player."""
+    moves = []
+
+    # Played and discard moves are easy
+    # Remember that our cards may be obfuscated
+    hand = game["players"][current_player]
+
+    if isinstance(hand[0], int):
+        card_ids = hand
+    else:
+        card_ids = [c[2] for c in hand]
+
+    for card_id in card_ids:
+        moves.append({"type": "play", "data": card_id})
+        moves.append({"type": "discard", "data": card_id})
+
+    # Want every possible clue
+    for pid, hand in game["players"].iteritems():
+        if pid != current_player:
+            for c in colours_in_hand(hand):
+                moves.append({"type": "clue", "data": (pid, c)})
+            for v in values_in_hand(hand):
+                moves.append({"type": "clue", "data": (pid, v)})
+
+    return moves
+
+def simulate(game, move):
+    """Simulate the given `move' on the current game to see if it is worth
+    making. This does not modify the game passed in, it returns a
+    modified copy.
+    """
+
+    def play_one_move(game, current_player, memory, user_args):
+        return move
+
+    # Make a copy
+    new_game = deepcopy(game)
+    new_game = play_one_turn(new_game,
+                             new_game["current_player"],
+                             play_one_move,
+                             None,
+                             None,
+                             False)
+
+    return new_game
+
+class HanabiProblem(SearchProblem):
+    def __init__(self, initial_game):
+        # Can't use the game as the state that gets passed around
+        # because it is a dictionary and that's not hashable. Can make
+        # it hashable but a hash would be too long, so it is easier to
+        # index into a list of game objects
+        super(HanabiProblem, self).__init__(0)
+        self.game_states = [initial_game]
+
+    def actions(self, game_id):
+        """this method receives the index into the list of game states
+        (`game_id'), and must return the list of moves that can be
+        performed from that particular game state.
+        """
+        game = self.game_states[game_id]
+        return all_moves(game, game["current_player"])
+
+    def result(self, game_id, move):
+        """this method receives the index into the list of game states and a
+        move, and must return the resulting state of applying that
+        particular move from that particular game state.
+        """
+        game = self.game_states[game_id]
+        next_game = simulate(game, move)
+        self.game_states.append(next_game)
+        return (len(self.game_states) - 1)
+
+    def is_goal(self, game_id):
+        """this method receives the index into the list of game states, and
+        must return True if the game state is a goal state, or False
+        if don't.
+        """
+        game = self.game_states[game_id]
+        return have_won(game)
 
 def play_move(game, current_player, memory, user_args):
     hand = game["players"][current_player]
@@ -170,10 +294,41 @@ def play_move(game, current_player, memory, user_args):
     # easier to debug
     return m
 
-if __name__ == "__main__":
-    # Iterate the algorithms to compare
-    for i in range(NUM_ALGORITHMS):
-        print "For algorithm %0d:" % i
-        play(100, 3, play_move, {"algorithm": i}, load_state = True)
-        print "-" * 80
+def play_move_ai(game, current_player, memory, user_args):
+    my_viewer = ConsoleViewer()
+    my_problem = HanabiProblem(game)
+    result = breadth_first(my_problem, viewer=my_viewer)
+    #result = breadth_first(my_problem)
+    print result.state
+    print print_game(result.game_states[result.state], -1, True)
+    print result.path()
 
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", type = str, choices = ["default", "ai"],
+                        default = "default", help = "Which move function to use.")
+    parser.add_argument("-a", type = int, default = 0,
+                        help = "Which algorithm to use of the move function. Default is to permute all.")
+    parser.add_argument("-n", type = int, default = 1,
+                        help = "How many times to play.")
+    parser.add_argument("-p", type = int, default = 3, choices = [2, 3, 4, 5],
+                        help = "How many players.")
+    parser.add_argument("-r", action = "store_true",
+                        help = "Re-run from the previous random seed.")
+    args = parser.parse_args()
+
+    if args.f == "ai":
+        play(args.n, args.p, play_move_ai, {}, load_state = args.r, obfuscate_game = False)
+    else:
+        if args.a == 0:
+            # Iterate the algorithms to compare
+            for i in range(NUM_ALGORITHMS):
+                print "For algorithm %0d:" % i
+                play(args.n, args.p, play_move, {"algorithm": i}, load_state = args.r)
+                print "-" * 80
+        else:
+                print "For algorithm %0d:" % args.a
+                play(args.n, args.p, play_move, {"algorithm": args.a}, load_state = args.r)
