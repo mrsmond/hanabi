ec04234222bac1a09851c7a45b4fd2c9f1b34644
Derek Graham <mrsmond@users.noreply.github.com>
2016-05-23 16:51:21 +0100

    Cleaned up to use function that builds hand possibilities.

--- hanabi_dgraham.py
+++ hanabi_dgraham.py
@@ -196,65 +196,21 @@ class HanabiProblem(SearchProblem):
 
 def play_move(game, current_player, memory, user_args):
     hand = game["players"][current_player]
-    
+
     # This stores the possible cards that each card in my hand could
-    # be. Need to use this syntax so we get a copy of FULL_SET iso a
-    # reference
-    my_hand = get_from(memory, "my_hand", dict([(id, list(FULL_SET)) for id in hand]))
+    # be and the possible cards that other players could have from my
+    # perspective (i.e. I can't see my hand)
+    #
+    # Easier to build this each turn instead of keeping it in memory.
+    all_hands = build_possible_hands(game, game["current_player"])
 
-    # This stores what possible cards are left in the deck
-    whats_left = get_from(memory, "whats_left", dict([(c, list(CARDS_PER_VALUE)) for c in COLOURS]))
-
-    # Keep track of what cards have been analysed
-    seen = get_from(memory, "seen", {})
-
-    # Use this to work out what new clues I need to process
-    last_clue = get_from(memory, "last_clue", -1)
-
+    my_hand = all_hands[current_player]
+    
     # A way to try out different algorithms
     clue_algorithm = get_from(user_args, "clue_algorithm", 0)
     discard_algorithm = get_from(user_args, "discard_algorithm", 0)
 
-    # Remove any cards I don't have anymore
-    old_ids = my_hand.keys()
-    for oid in old_ids:
-        if not oid in hand:
-            del my_hand[oid]
-
-    # Initialise any new cards
-    for id in hand:
-        if not id in my_hand:
-            my_hand[id] = list(FULL_SET)   # take a copy
-
-    # Update what's left by first checking other player's hands,
-    # making sure not to look at my hand and to only examine those
-    # I've not seen.
-    # I do have to look at the discarded and played piles because I
-    # may have put a card there.
-    # chain.from_iterable flattens all the hands into one list and
-    # chain joins the lists into one iterator
     other_players_cards = list(chain.from_iterable([h for p, h in game["players"].iteritems() if p != current_player]))
-    all_cards = other_players_cards + game["played"] + game["discarded"]
-    for (colour, value, id) in all_cards:
-        if not id in seen:
-            whats_left[colour].remove(value)
-            seen[id] = True
-
-    # Rule out from my hand any card not possible from what's left
-    for colour, values in whats_left.iteritems():
-        for v in VALUES:
-            if not v in values:
-                rule_out(my_hand, colour, v)
-
-    # Update my hand with any clues given to me since last time
-    for clue_data in [m[1]["data"] for i, m in enumerate(game["moves"]) if m[1]["type"] == "clue" and \
-                 m[1]["data"][0] == current_player and \
-                 i > last_clue]:
-        prune(my_hand, clue_data[2], clue_data[1])
-
-    # Easiest to just set it to the last move as we only process clues
-    # above
-    last_clue = len(game["moves"]) - 1
 
     # Now we have worked out what cards I might have, let's see if any
     # are playable
